b288fdd1f4844de359e3b46dd346f6d2b69e499f
Matt Latham <github@mattlatham.dev>
2023-01-14 08:05:54 -0800

    add ad

--- pages/ad.vue
+++ pages/ad.vue
@@ -1,7 +1,19 @@
 <template>
-  <div></div>
+  <iframe
+    allow="accelerometer; autoplay; encrypted-media; fullscreen; gyroscope; picture-in-picture"
+    allowfullscreen
+    class="pdf-viewer"
+    scrolling="no"
+    :src="url"
+  />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const url = 'https://91d208c3-trial.flowpaper.com/p1011122PFFlowPaperJoined/'
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.pdf-viewer {
+  @apply h-full w-full min-h-[400px];
+}
+</style>
--- pages/products/order.vue
+++ pages/products/order.vue
@@ -21,7 +21,7 @@
     </AppTypography>
 
     <form @submit.prevent="onSubmit">
-      <div class="grid grid-cols-2 gap-8">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <section>
           <h1>Item Information</h1>
 
