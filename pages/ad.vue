611865f33dac4987113ed0c02823cb018b5ad075
Matt Latham <github@mattlatham.dev>
2023-02-04 10:58:38 -0800

    adds flowpaper

--- pages/ad.vue
+++ pages/ad.vue
@@ -1,15 +1,60 @@
 <template>
-  <iframe
-    allow="accelerometer; autoplay; encrypted-media; fullscreen; gyroscope; picture-in-picture"
-    allowfullscreen
-    class="pdf-viewer"
-    scrolling="no"
-    :src="url"
-  />
+  <div>
+    <div class="relative">
+      <div id="documentViewer"></div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-const url = 'https://91d208c3-trial.flowpaper.com/p1011122PFFlowPaperJoined/'
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: '/flowpaper/css/flowpaper.css',
+    },
+  ],
+  script: [
+    { key: 'jquery.min.js', src: '/flowpaper/js/jquery.min.js' },
+    {
+      key: 'jquery.extensions.min.js',
+      src: '/flowpaper/js/jquery.extensions.min.js',
+    },
+    { key: 'three.min.js', src: '/flowpaper/js/three.min.js' },
+    { key: 'flowpaper.js', src: '/flowpaper/js/flowpaper.js' },
+    {
+      key: 'flowpaper_handlers.js',
+      src: '/flowpaper/js/flowpaper_handlers.js',
+    },
+  ],
+})
+
+onMounted(() => {
+  function doTheThing() {
+    console.log(window.jQuery)
+    window.jQuery('#documentViewer').FlowPaperViewer({
+      config: {
+        jsDirectory: '/flowpaper/js/',
+        cssDirectory: '/flowpaper/css/',
+        localeDirectory: '/flowpaper/locale/',
+        PDFFile: '/flowpaper/pdf/Binder1.pdf',
+        EnableWebGL: true,
+        UIConfig: '/flowpaper/UI_Zine.xml',
+        FitPageOnLoad: true,
+        FitWidthOnLoad: true,
+        ProgressiveLoading: false,
+      },
+    })
+  }
+
+  if (window.jQuery) {
+    doTheThing()
+  } else {
+    window.addEventListener('load', () => {
+      doTheThing()
+    })
+  }
+})
 </script>
 
 <style scoped lang="scss">
