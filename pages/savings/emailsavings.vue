074514b5c93f93f48e2dae60f36e4eeb30012f8b
Matt Latham <6905481+notafunction@users.noreply.github.com>
2023-03-14 22:40:23 -0700

    debug

--- pages/savings/emailsavings.vue
+++ pages/savings/emailsavings.vue
@@ -257,6 +257,7 @@ const onSubmit = handleSubmit(
       turnstileRef.value.reset()
       useTrackEvent('esp_form_submission')
     } catch (error) {
+      console.log(error)
       toast.error((error as FetchError<H3Error>).message)
     }
   },
