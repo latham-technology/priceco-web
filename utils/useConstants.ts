1c68207135832c8010e4fbf901248ad5a7972840
Matt Latham <6905481+notafunction@users.noreply.github.com>
2023-03-14 22:59:29 -0700

    fix typo in error message

--- utils/useConstants.ts
+++ utils/useConstants.ts
@@ -3,9 +3,9 @@ export const useConstants = () => ({
   APP_EMPLOYMENT_SUBMIT_SUCCESS: 'Thank you for applying!',
   APP_ITEM_ORDER_SUBMIT_SUCCESS: 'Thank you',
   APP_CONTACT_SUBMIT_SUCCESS: 'Thank you for your feedback!',
-  APP_FORM_VALIDATION_ERROR: 'Please check the missing fields and try again',
+  APP_FORM_VALIDATION_ERROR: 'Please check the missing fields and try again.',
 
   API_TURNSTILE_VERIFICATION_FAILED:
-    'The anti-spam token failed verification, please try again',
-  API_ESP_RECORD_EXISTS: 'That phone number has already been registerd',
+    'The anti-spam token failed verification. Please refresh the page and try again.',
+  API_ESP_RECORD_EXISTS: 'That phone number has already been registered.',
 })
