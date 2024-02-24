f0e8c10190b2255b6c70c834489c5e0eb9bd1271
Matt Latham <github@mattlatham.dev>
2024-02-23 23:18:51 -0800

    add labels

--- app/server/schemas/application.ts
+++ app/server/schemas/application.ts
@@ -1,46 +1,52 @@
 import { object, string, boolean, array } from 'yup'
 
 export const personalSchema = object().shape({
-    firstName: string().required(),
-    lastName: string().required(),
-    email: string().email().required(),
-    phone: string().required().min(14),
-    address1: string().required(),
-    address2: string(),
-    city: string().required(),
-    state: string().required(),
-    zip: string().required().min(5),
+    firstName: string().required().label('First Name'),
+    lastName: string().required().label('Last Name'),
+    email: string().email().required().label('Email'),
+    phone: string().required().min(14).label('Phone number'),
+    address1: string().required().label('Address Line 1'),
+    address2: string().label('Address Line 2'),
+    city: string().required().label('City'),
+    state: string().required().label('State'),
+    zip: string().required().min(5).label('Postal code'),
 })
 
 export const positionSchema = object().shape({
-    desired: string().required(),
-    dateAvailable: string().required(),
-    availability: string().nullable().required(),
-    salary: string().required(),
-    currentlyEmployed: boolean().nullable().required(),
+    desired: string().required().label('Desired Position'),
+    dateAvailable: string().required().label('Date Available'),
+    availability: string()
+        .nullable()
+        .required()
+        .label('Availability'),
+    salary: string().required().label('Salary Desired'),
+    currentlyEmployed: boolean()
+        .nullable()
+        .required()
+        .label('Currently Employed'),
 })
 
 export const educationSchema = object().shape({
-    type: string().nullable().required(),
-    name: string().required(),
-    location: string(),
-    subjects: string(),
-    complete: boolean().nullable().required(),
+    type: string().nullable().required().label('Type'),
+    name: string().required().label('Name'),
+    location: string().label('Location'),
+    subjects: string().label('Subjects'),
+    complete: boolean().nullable().required().label('Completed'),
 })
 
 export const historySchema = object().shape({
-    name: string().required(),
-    title: string().required(),
-    location: string().required(),
-    datesEmployed: string().required(),
-    leaveReason: string().required(),
+    name: string().required().label('Name'),
+    title: string().required().label('Title'),
+    location: string().required().label('Location'),
+    datesEmployed: string().required().label('Dates Employed'),
+    leaveReason: string().required().label('Leave Reason'),
 })
 
 export const referenceSchema = object().shape({
-    name: string().required(),
-    yearsKnown: string().required(),
-    address: string(),
-    phone: string().required(),
+    name: string().required().label('Name'),
+    yearsKnown: string().required().label('Years Known'),
+    phone: string().required().label('Phone'),
+    address: string().label('Address'),
 })
 
 export default object().shape({
